e658b929a24d1c303cdaba21227ed42d5a5ed42d
QuantumDuck <40206399+BrokenDuck@users.noreply.github.com>
2024-12-03 20:22:15 +0100

    Big fix

--- graph.py
+++ graph.py
@@ -34,24 +34,23 @@ def scoping(state):
     logging.info("---SCOPE---")
     question = state["question"]
     
-    if not hasattr(scoping, "scoper"):
-        class QueryScope(BaseModel):
-            """Decide on the scope of the query."""
-            
-            scope: Literal["constitution", "movementforchange", "ndc", "npp", "thenewforce", "all"] = Field(
-                ...,
-                description="Given a user question choose to route it either to specific party in the Ghana 2024 elections, either to the constitution or just to look through all the documents.",
-            )
-        system = """You are an expert at deciding if a question refers to a specific party in Ghana or to its constitution. If the question refers to the Constitution of Ghana, use constitution. If the question refers to the New Patriotic Party (NPP) party, use npp. If the question refers to the National Democratic Congress (NDC) party, use ndc. If the question refers to the Movement for Change party, use movementforchange. If the question refers to The New Force party, use thenewforce. If the question refers to multiple parties, doesn't refer to one party in particular, or if cannot answer the question, you don't know or cannot decide, use all.\n"""
-        route_prompt = ChatPromptTemplate.from_messages(
-            [
-                ("system", system),
-                ("human", "Question: {question}")
-            ]
+    class QueryScope(BaseModel):
+        """Decide on the scope of the query."""
+        
+        scope: Literal["constitution", "movementforchange", "ndc", "npp", "thenewforce", "all"] = Field(
+            ...,
+            description="Given a user question choose to route it either to specific party in the Ghana 2024 elections, either to the constitution or just to look through all the documents.",
         )
-        route_question.question_router = route_prompt | get_llm().with_structured_output(QueryScope)
+    system = """You are an expert at deciding if a question refers to a specific party in Ghana or to its constitution. If the question refers to the Constitution of Ghana, use constitution. If the question refers to the New Patriotic Party (NPP) party, use npp. If the question refers to the National Democratic Congress (NDC) party, use ndc. If the question refers to the Movement for Change party, use movementforchange. If the question refers to The New Force party, use thenewforce. If the question refers to multiple parties, doesn't refer to one party in particular, or if cannot answer the question, you don't know or cannot decide, use all.\n"""
+    route_prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", system),
+            ("human", "Question: {question}")
+        ]
+    )
+    question_router = route_prompt | get_llm().with_structured_output(QueryScope)
     
-    scope = route_question.question_router.invoke({"question": question})
+    scope = question_router.invoke({"question": question})
     return {"question": question, "scope": scope.scope}
 
 def retrieve(state):
@@ -69,29 +68,28 @@ def retrieve(state):
     scope = state["scope"].lower()
     
     # ensure retriever is define
-    if not hasattr(retrieve, "retriever"):
-        retrieve.retriever = AzureAISearchRetriever(
-            api_version="2024-07-01",
-            content_key="chunk",
-            top_k=5
-        )
+    retriever = AzureAISearchRetriever(
+        api_version="2024-07-01",
+        content_key="chunk",
+        top_k=5
+    )
         
     # Set Filter
     if scope == "constitution":
-        retrieve.retriever.filter = "title eq 'constitution.pdf'"
+        retriever.filter = "title eq 'constitution.pdf'"
     elif scope == "npp":
-        retrieve.retriever.filter = "title eq 'npp.pdf'"
+        retriever.filter = "title eq 'npp.pdf'"
     elif scope == "ndc":
-        retrieve.retriever.filter = "title eq 'ndc.pdf'"
+        retriever.filter = "title eq 'ndc.pdf'"
     elif scope == "movementforchange":
-        retrieve.retriever.filter = "title eq 'movementforchange.pdf'"
+        retriever.filter = "title eq 'movementforchange.pdf'"
     elif scope == "thenewforce":
-        retrieve.retriever.filter = "title eq 'thenewforce.pdf'"
+        retriever.filter = "title eq 'thenewforce.pdf'"
     else:
-        retrieve.retriever.filter = None
+        retriever.filter = None
 
     # Retrieval
-    documents = retrieve.retriever.invoke(question)
+    documents = retriever.invoke(question)
     return {"documents": documents, "scope": scope, "question": question}
 
 
@@ -111,19 +109,18 @@ def generate(state):
     scope = state["scope"]
     
     # Ensure RAG chain is built
-    if not hasattr(generate, "rag_chain"):
-        system = """You are an assistant for question-answering tasks. Use the following pieces of retrieved context to answer the question. If you don't know the answer, just say that you don't know. Use three sentences maximum and keep the answer concise."""
-        rag_prompt = ChatPromptTemplate.from_messages(
-            [
-                ("system", system),
-                ("human", "Questions: {question} \n Context: \n\n {context} \n Answer:")
-            ]
-        )
-        generate.rag_chain = rag_prompt | get_llm() | StrOutputParser()
+    system = """You are an assistant for question-answering tasks. Use the following pieces of retrieved context to answer the question. If you don't know the answer, just say that you don't know. Use three sentences maximum and keep the answer concise."""
+    rag_prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", system),
+            ("human", "Questions: {question} \n Context: \n\n {context} \n Answer:")
+        ]
+    )
+    rag_chain = rag_prompt | get_llm() | StrOutputParser()
         
 
     # RAG generation
-    generation = generate.rag_chain.invoke({"context": documents, "question": question})
+    generation = rag_chain.invoke({"context": documents, "question": question})
     return {"documents": [document.page_content for document in documents], "scope": scope, "question": question, "generation": generation}
 
 
@@ -143,26 +140,25 @@ def grade_documents(state):
     documents = state["documents"]
     
     # Ensure grader is built
-    if not hasattr(grade_documents, "retrieval_grader"):
-        class GradeDocuments(BaseModel):
-            """Binary score for relevance check on retrieved documents."""
+    class GradeDocuments(BaseModel):
+        """Binary score for relevance check on retrieved documents."""
 
-            binary_score: str = Field(
-                description="Documents are relevant to the question, 'yes' or 'no'"
-            )
-        system = """You are a grader assessing relevance of a retrieved document to a user question. If the document contains keyword(s) or semantic meaning related to the user question, grade it as relevant. It does not need to be a stringent test. The goal is to filter out erroneous retrievals. Give a binary score 'yes' or 'no' score to indicate whether the document is relevant to the question."""
-        grade_prompt = ChatPromptTemplate.from_messages(
-            [
-                ("system", system),
-                ("human", "Retrieved document: \n\n {document} \n\nUser Question: {question}"),
-            ]
+        binary_score: str = Field(
+            description="Documents are relevant to the question, 'yes' or 'no'"
         )
-        grade_documents.retrieval_grader = grade_prompt | get_llm().with_structured_output(GradeDocuments)
+    system = """You are a grader assessing relevance of a retrieved document to a user question. If the document contains keyword(s) or semantic meaning related to the user question, grade it as relevant. It does not need to be a stringent test. The goal is to filter out erroneous retrievals. Give a binary score 'yes' or 'no' score to indicate whether the document is relevant to the question."""
+    grade_prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", system),
+            ("human", "Retrieved document: \n\n {document} \n\nUser Question: {question}"),
+        ]
+    )
+    retrieval_grader = grade_prompt | get_llm().with_structured_output(GradeDocuments)
         
     # Score each doc
     filtered_docs = []
     for d in documents:
-        score = grade_documents.retrieval_grader.invoke(
+        score = retrieval_grader.invoke(
             {"question": question, "document": d.page_content}
         )
         grade = score.binary_score
@@ -191,21 +187,20 @@ def transform_query(state):
     documents = state["documents"]
     
     # Ensure that question re-writer is built
-    if not hasattr(transform_query, "question_rewriter"):
-        system = """You are a question re-writer that converts an input question to a better version that is optimized for vectorstore retrieval. Look at the input and try to reason about the underlying semantic intent / meaning."""
-        re_write_prompt = ChatPromptTemplate.from_messages(
-            [
-                ("system", system),
-                (
-                    "human",
-                    "Here is the initial question: \n\n {question} \n\nFormulate an improved question.",
-                )
-            ]
-        )
-        transform_query.question_rewriter = re_write_prompt | get_llm() | StrOutputParser()
+    system = """You are a question re-writer that converts an input question to a better version that is optimized for vectorstore retrieval. Look at the input and try to reason about the underlying semantic intent / meaning."""
+    re_write_prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", system),
+            (
+                "human",
+                "Here is the initial question: \n\n {question} \n\nFormulate an improved question.",
+            )
+        ]
+    )
+    question_rewriter = re_write_prompt | get_llm() | StrOutputParser()
         
     # Re-write question
-    better_question = transform_query.question_rewriter.invoke({"question": question})
+    better_question = question_rewriter.invoke({"question": question})
     return {"documents": documents, "question": better_question}
 
 ### Edges ###
@@ -225,25 +220,24 @@ def route_question(state):
     question = state["question"]
     
     # Ensure question router is instantiated
-    if not hasattr(route_question, "question_router"):
-        class RouteQuery(BaseModel):
-            """Route a user query to the most relevant datasource."""
-            
-            relevant: Literal["yes", "no"] = Field(
-                ...,
-                description="Given a user question choose if it is relevant in any way to the 2024 Ghana elections.",
-            )
-        system = """You are an expert at deciding if a question is relevant to the Ghana or elections. Decide broadly and don't be stringent about the conditions. If the question is in any way related to Ghana, decide yes. Otherwise, decide no.\n\n"""
-        route_prompt = ChatPromptTemplate.from_messages(
-            [
-                ("system", system),
-                ("human", "Question: {question}")
-            ]
-        )
-        route_question.question_router = route_prompt | get_llm().with_structured_output(RouteQuery)
+    class RouteQuery(BaseModel):
+        """Route a user query to the most relevant datasource."""
         
-    source = route_question.question_router.invoke({"question": question})
-    print(source)
+        relevant: Literal["yes", "no"] = Field(
+            ...,
+            description="Given a user question choose if it is relevant in any way to the 2024 Ghana elections.",
+        )
+    system = """You are an expert at deciding if a question is relevant to the Ghana or elections. Decide broadly and don't be stringent about the conditions. If the question is in any way related to Ghana, decide yes. Otherwise, decide no.\n\n"""
+    route_prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", system),
+            ("human", "Question: {question}")
+        ]
+    )
+    question_router = route_prompt | get_llm().with_structured_output(RouteQuery)
+        
+    source = question_router.invoke({"question": question})
+    logging.info(source)
     if source.relevant == "yes":
         logging.info("---QUESTION IS RELEVANT---")
         return "yes"
@@ -297,41 +291,39 @@ def grade_generation_v_documents_and_question(state):
     generation = state["generation"]
     
     # Ensure that hallucination grader is instantiated
-    if not hasattr(grade_generation_v_documents_and_question, "hallucination_grader"):
-        class GradeHallucinations(BaseModel):
-            """Binary score for hallucination present in generation answer."""
+    class GradeHallucinations(BaseModel):
+        """Binary score for hallucination present in generation answer."""
 
-            binary_score: str = Field(
-                description="Answer is grounded in the facts, 'yes' or 'no'"
-            )
-        system = """You are a grader assessing whether an LLM generation is grounded in / supported by a set of retrieved facts. Give a binary score 'yes' or 'no'. 'Yes' means that the answer is grounded in / supported by the set of facts."""
-        hallucination_prompt = ChatPromptTemplate.from_messages(
-            [
-                ("system", system),
-                ("human", "Set of facts: \n\n {documents} \n\n LLM generation: {generation}"),
-            ]
+        binary_score: str = Field(
+            description="Answer is grounded in the facts, 'yes' or 'no'"
         )
-        grade_generation_v_documents_and_question.hallucination_grader = hallucination_prompt | get_llm().with_structured_output(GradeHallucinations)
+    system = """You are a grader assessing whether an LLM generation is grounded in / supported by a set of retrieved facts. Give a binary score 'yes' or 'no'. 'Yes' means that the answer is grounded in / supported by the set of facts."""
+    hallucination_prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", system),
+            ("human", "Set of facts: \n\n {documents} \n\n LLM generation: {generation}"),
+        ]
+    )
+    hallucination_grader = hallucination_prompt | get_llm().with_structured_output(GradeHallucinations)
 
     # Ensure that answer grader is instantiated
-    if not hasattr(grade_generation_v_documents_and_question, "answer_grader"):
-        class GradeAnswer(BaseModel):
-            """Binary score to assess wether the answer addresses the question."""
+    class GradeAnswer(BaseModel):
+        """Binary score to assess wether the answer addresses the question."""
 
-            binary_score: str = Field(
-                description="Answer addresses the question, 'yes' or 'no'"
-            )
-        system = """You are a grader assessing whether an answer addresses / resolves a question. Give a binary score 'yes' or 'no'. Yes' means that the answer resolves the question."""
-        answer_prompt = ChatPromptTemplate.from_messages(
-            [
-                ("system", system),
-                ("human", "User question: \n\n {question} \n\n LLM generation: {generation}"),
-            ]
+        binary_score: str = Field(
+            description="Answer addresses the question, 'yes' or 'no'"
         )
-        
-        grade_generation_v_documents_and_question.answer_grader = answer_prompt | get_llm().with_structured_output(GradeAnswer)
+    system = """You are a grader assessing whether an answer addresses / resolves a question. Give a binary score 'yes' or 'no'. Yes' means that the answer resolves the question."""
+    answer_prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", system),
+            ("human", "User question: \n\n {question} \n\n LLM generation: {generation}"),
+        ]
+    )
+    
+    answer_grader = answer_prompt | get_llm().with_structured_output(GradeAnswer)
 
-    score = grade_generation_v_documents_and_question.hallucination_grader.invoke(
+    score = hallucination_grader.invoke(
         {"documents": "\n\n".join(documents), "generation": generation}
     )
     grade = score.binary_score
@@ -341,7 +333,7 @@ def grade_generation_v_documents_and_question(state):
         logging.info("---DECISION: GENERATION IS GROUNDED IN DOCUMENTS---")
         # Check question-answering
         logging.info("---GRADE GENERATION vs QUESTION---")
-        score = grade_generation_v_documents_and_question.answer_grader.invoke({"question": question, "generation": generation})
+        score = answer_grader.invoke({"question": question, "generation": generation})
         grade = score.binary_score
         if grade == "yes":
             logging.info("---DECISION: GENERATION ADDRESSES QUESTION---")
